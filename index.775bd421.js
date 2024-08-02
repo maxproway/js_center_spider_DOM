7af5a9e26b6e1cd4e746356c85c54e2b8646739a
Sergiy Dolgov <info@devmax.pro>
2024-08-02 07:32:46 -0400

    make build

---
+++ index.775bd421.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=e.clientWidth,i=e.clientHeight,o=t.offsetWidth,c=t.offsetHeight;t.style.left=l/2-o/2+"px",t.style.top=i/2-c/2+"px";
+//# sourceMappingURL=index.775bd421.js.map
